1268f5cb7a9677332a07b1c571f0a2a407b14348
guili liu <liuguili@juzi.bot>
2022-02-28 16:27:35 +0800

    fix: add corp tag dto tag type

--- src/externalcontact/corp-tag/add-corp-tag-dto.ts
+++ src/externalcontact/corp-tag/add-corp-tag-dto.ts
@@ -49,7 +49,7 @@ export class AddCorpTagDto {
   })
   order: number;
 
-  @ApiProperty({ type: AddCorpTagDto, required: false })
+  @ApiProperty({ type: AddCorpTagItem, required: false })
   tag: AddCorpTagItem;
 
   @ApiProperty({
