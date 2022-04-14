84a34ad132dacc719ee519ff30460bc13c494d42
guili liu <liuguili@juzi.bot>
2022-04-14 11:41:10 +0800

    fix: ListContactWayDto set number format to int64

--- src/corp-internal/externalcontact/contact-way/list-contact-way-dto.ts
+++ src/corp-internal/externalcontact/contact-way/list-contact-way-dto.ts
@@ -3,6 +3,7 @@ import { ApiProperty } from '@nestjs/swagger';
 export class ListContactWayDto {
   @ApiProperty({
     type: 'number',
+    format: 'int64',
     required: false,
     description: '「联系我」创建起始时间戳, 默认为90天前',
     example: 1622476800,
@@ -11,6 +12,7 @@ export class ListContactWayDto {
 
   @ApiProperty({
     type: 'number',
+    format: 'int64',
     required: false,
     description: '「联系我」创建结束时间戳, 默认为当前时间',
     example: 1625068800,
@@ -27,6 +29,7 @@ export class ListContactWayDto {
 
   @ApiProperty({
     type: 'number',
+    format: 'int64',
     required: false,
     description: '每次查询的分页大小，默认为100条，最多支持1000条',
     example: 1000,
