1bba09929d78cdbb2b584bef45621d16e72cd5c3
zelong zhang <1759279542@qq.com>
2022-04-28 20:54:31 +0800

    增加authentication openapi

---
+++ src/corp-internal/authentication/authentication.controller.ts
@@ -0,0 +1,51 @@
+import { Controller, Get, Post } from '@nestjs/common';
+import {
+    ApiOkResponse,
+    ApiOperation,
+    ApiQuery,
+    ApiTags,
+} from '@nestjs/swagger';
+import {ScanAuthenticationResponse} from "./scan-authentication-response";
+import {WebAuthenticationResponse} from "./web-authentication-response";
+
+@ApiTags('authentication')
+@Controller('authentication')
+export class AuthenticationController {
+    @Get('getuserinfo')
+    @ApiOperation({
+        operationId: 'web getuserinfo',
+        summary: '获取web访问用户身份',
+        externalDocs: {
+            url: 'https://developer.work.weixin.qq.com/document/path/91023',
+        },
+    })
+    @ApiQuery({
+        name: 'code',
+        required: true,
+        type: 'string',
+        description: '通过成员授权获取到的code',
+    })
+    @ApiOkResponse({ type: ScanAuthenticationResponse })
+    public getuserinfo() {
+        return null;
+    }
+
+    @Get('get')
+    @ApiOperation({
+        operationId: 'scan getuserinfo',
+        summary: '获取扫码访问用户身份',
+        externalDocs: {
+            url: 'https://developer.work.weixin.qq.com/document/path/91437',
+        },
+    })
+    @ApiQuery({
+        name: 'code',
+        required: true,
+        type: 'string',
+        description: '通过成员授权获取到的code',
+    })
+    @ApiOkResponse({ type: WebAuthenticationResponse })
+    public get() {
+        return null;
+    }
+}
