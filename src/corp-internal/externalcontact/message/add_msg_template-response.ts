7721fda2999700526fa49cda5face4e1a4fe8261
hamlin.li <sclihuiming@163.com>
2022-11-11 11:26:18 +0800

    perf: getGroupMsgSendResultResponse

--- src/corp-internal/externalcontact/message/add_msg_template-response.ts
+++ src/corp-internal/externalcontact/message/add_msg_template-response.ts
@@ -40,8 +40,13 @@ export class SendList {
   userid: string;
 
   @ApiProperty({
+    enum: [0, 1, 2, 3],
     type: 'number',
+    format: 'int64',
     required: false,
+    description:
+      '发送状态：0-未发送 1-已发送 2-因客户不是好友导致发送失败 3-因客户已经收到其他群发消息导致发送失败',
+    example: 1,
   })
   status: number;
 
